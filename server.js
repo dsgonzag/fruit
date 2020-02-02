6e11248ec536fff3ca4fb11403643092826e0571
Diana Gonzaga <45009733+dsgonzag@users.noreply.github.com>
2020-02-02 17:52:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,87 +1,30 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('fruit.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+var http = require('http'),
+express  = require('express'),
+bodyParser   = require('body-parser');
 
-server.use(middlewares);
-server.use(router);
+var multer = require('multer'); 
+const pg    = require('pg');
 
-server.listen(port);
+pg.defaults.ssl = true;
+var conString = "postgres://vndkgmnovwcudz:bd0da1d0c70efdea9f06be6f4e859b17d89f9202f3754ab66384693942ffb4bf@ec2-174-129-33-186.compute-1.amazonaws.com:5432/da6r9929umk6ro";
+var express = require('express');
+var http = require('http'),
+    formidable = require('formidable'),
+    util = require('util'),
+    fs   = require('fs-extra');
+    
+    
+function permitirCrossDomain(req, res, next) {
+        //en vez de * se puede definir SÓLO los orígenes que permitimos
+        res.header('Access-Control-Allow-Origin', '*'); 
+        //metodos http permitidos para CORS
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+      }  
+      
+      var app = express();
 
-<<<<<<< HEAD
-// Cargar modulos y crear nueva aplicacion
-var express = require("express"); 
-var cors = require('cors')
-var app = express();
-app.use(cors())
- 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
- 
-//GetAll
-//Ejemplo: GET http://localhost:8080/item
-app.get('/frutas', function(req, res, next) {
-  if(req.query.filter) {
-   next();
-   return;
-  }
-  res.send('Get all');
-  console.log('Get all');
-});
- 
-//GetById
-//Ejemplo: GET http://localhost:8080/item/10
-app.get('/frutas/:id', function(req, res, next) {
-  var id = req.params.id;
-  res.send('Get ' + req.params.id);
-  console.log('Get ' + req.params.id);
-});
- 
-//GetFiltered
-//Ejemplo: GET http://localhost:8080/item?filter=ABC
-app.get('/frutas', function(req, res) {
-  var filter = req.query.filter;
-  res.send('Get filter ' + filter);
-  console.log('Get filter ' + filter);
-});
- 
-//Create
-//Ejemplo: POST http://localhost:8080/item
-app.post('/frutas', function(req, res) {
-   var data = req.body.data;
-   res.send('Add ' + data);
-   console.log('Add ' + data);
-});
- 
-//Replace
-//Ejemplo: PUT http://localhost:8080/item/10
-app.put('/frutas/:id', function(req, res) {
-   var id = req.params.id;
-   var data = req.body.data;
-   res.send('Replace ' + id + ' with ' + data);
-   console.log('Replace ' + id + ' with ' + data);
-});
- 
-//Update
-//Ejemplo: PATCH http://localhost:8080/item/10
-app.patch('/frutas/:id', function(req, res) {
-   var id = req.params.id;
-   var data = req.body.data;
-   res.send('Update ' + id + ' with ' + data);
-   console.log('Update ' + id + ' with ' + data);
-});
- 
-//Delete
-//Ejemplo: DEL http://localhost:8080/items
-app.delete('/frutas/:id', function(req, res) {
-   var id = req.params.id;
-   res.send('Delete ' + id);
-   console.log('Delete ' + id);
-});
-  
-=======
       app.use(bodyParser.json()); // for parsing application/json
       app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
       
@@ -93,7 +36,7 @@ app.delete('/frutas/:id', function(req, res) {
         res.send('hello world');
       });
 
-      app.get('/listFruits', (req, res, next) => {
+      app.get('/listUsers', (req, res, next) => {
         var client = new pg.Client(conString);
         client.connect(function(err) {
             if(err) {
@@ -113,7 +56,7 @@ app.delete('/frutas/:id', function(req, res) {
         });
     });
     
-    app.get('/listFruit/:id',(req,res)=>{
+    app.get('/listUser/:id',(req,res)=>{
         var client = new pg.Client(conString);
         var id=req.params.id;
     
@@ -136,7 +79,7 @@ app.delete('/frutas/:id', function(req, res) {
             
         });
     });
-    app.put('/updateFruit',(req,res)=>{
+    app.put('/updateUser',(req,res)=>{
         var client = new pg.Client(conString);
         var id=req.body.id;
         client.connect(function(err) {
@@ -145,7 +88,7 @@ app.delete('/frutas/:id', function(req, res) {
                 return res.status(500).json({success: false, data: err});
             }
     
-            client.query("UPDATE frutas SET nombre_fruta='"+req.body.nombre_fruta+"',cantidad='"+req.body.cantidad+"' WHERE id='" + id + "';", function(err, result) {
+            client.query("UPDATE usuario SET nombre='"+req.body.nombre+"',mail='"+req.body.mail+"', nom_user='"+req.body.nom_user+"',pass='"+req.body.pass+"' WHERE id='" + id + "';", function(err, result) {
                 
                 if(err) {
                     return console.error('error running query', err);
@@ -157,7 +100,7 @@ app.delete('/frutas/:id', function(req, res) {
             });
         });
     });
-    app.post('/SaveFruit', (req, res) => {
+    app.post('/SaveUser', (req, res) => {
         var client = new pg.Client(conString);
         client.connect(function(err) {
             if(err) {
@@ -167,7 +110,7 @@ app.delete('/frutas/:id', function(req, res) {
             
             console.log("miau "+util.inspect(req,false,null));
             
-            client.query("INSERT INTO  frutas  (nombre_fruta, cantidad) VALUES ('"+req.body.nombre_fruta+"', '"+req.body.cantidad+"');", function(err, result) {
+            client.query("INSERT INTO  usuario  (nombre,mail,nom_user,pass) VALUES ('"+req.body.nombre+"', '"+req.body.mail+"', '"+req.body.nom_user+"', '"+req.body.pass+"');", function(err, result) {
                 if(err) {
                     return console.error('error running query', err);
                 }
@@ -180,7 +123,7 @@ app.delete('/frutas/:id', function(req, res) {
             
         });
     });
-    app.delete('/deleteFruit',(req,res)=>{
+    app.delete('/deleteUser',(req,res)=>{
         var client = new pg.Client(conString);
         var id=req.body.id;
     
@@ -206,4 +149,3 @@ app.delete('/frutas/:id', function(req, res) {
     });
     
     app.listen(process.env.PORT || 8080, function(){console.log("the server is running");});
->>>>>>> 384f74f00515a042c3238bdb21eb608efc832700
